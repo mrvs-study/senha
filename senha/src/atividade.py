8c64195f56d881c45b3e5106d09c17473da4f6d7
marcos <mvsb2@discente.ifpe.edu.br>
2023-11-06 21:32:11 -0300

    update py

--- senha/src/atividade.py
+++ senha/src/atividade.py
@@ -1,16 +1,30 @@
-def nome():
-    aluno=input("Digite seu nome: ")
-    return aluno
-pp=nome()
+#crie um jogo de adivinha numero com uma matriz e os jogadores disponiveis
+import random
 
-def soma_media():
-   nota1=float (input("digite sua 1 nota: "))
-   
-   nota2=float (input("digite sua 2 nota: "))
-   
-   soma = (nota1+nota2)/2
-   return soma
+print("BEM-VINDO AO JOGO DO CHUTE")
 
-media_aluno = soma_media()
+matriz=[["joão"],
+["carmem"],
+["pedro"],
+["maria"]]
 
-print("A media do aluno",pp,"é:",media_aluno)
+escolha=float(input("Nos diga qual jogador você é? 0,1,2,3(0=João, 1=Carmem, 2=Pedro, 3=Maria)"))
+if (escolha==0):
+    escolha=matriz[0]
+    print(matriz[0])
+elif (escolha==1):
+    escolha=matriz[1]
+    print(matriz[1])
+elif (escolha==2):
+    escolha=matriz[2]
+    print(matriz[2])
+elif (escolha==3):
+    escolha=matriz[3]
+    print(matriz[3])
+    
+respota=float(input("digite o seu valor de chute: "))
+if respota==random.randint(0,9):
+    print("o vencedor(a) é:", escolha)
+    print("com o numero:",respota)
+else: print("você não venceu, tente outra vez")
+
